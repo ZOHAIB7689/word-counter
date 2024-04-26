8c131000e932a99b526cfd533a3514dcc3a7dead
ZOHAIB7689 <bzohaib754@gmail.com>
2024-04-26 13:32:31 +0500

    lexi-score

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+// Declare a constant 'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt({
+    name: "Sentence",
+    type: "input",
+    message: "enter your sentence to count the word "
+});
+const words = answers.Sentence.trim().split(" ");
+// Print the array of words to console 
+// Print the word of the sentence to the console
+console.log(`Your sentence word count is${words.length}`);
